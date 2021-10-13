af314cf84d917405baa1f933b7058903d45bfc01
EC2 Default User <ec2-user@ip-172-31-2-230.us-east-2.compute.internal>
2021-10-13 20:33:27 +0000

    Add spec and some part 2

--- rottenpotatoes/features/support/paths.rb
+++ rottenpotatoes/features/support/paths.rb
@@ -14,13 +14,15 @@ module NavigationHelpers
     case page_name
 
     when /^the (RottenPotatoes )?home\s?page$/ then '/movies'
-
+    movies_path
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
     #   when /^(.*)'s profile page$/i
     #     user_profile_path(User.find_by_login($1))
-
+    when /^the Similar Movies page for "(.+)"/
+      search_similar_movies_path($1)
+      
     else
       begin
         page_name =~ /^the (.*) page$/
